128f203524e2ff70c626083d8d25220245d7cdd5
Jay Moon <jaymoon9876@gmail.com>
2020-03-02 21:39:45 -0500

    Mouse hover on the map opens a popup with a precinct name

--- gui-prototype/src/Edit.js
+++ gui-prototype/src/Edit.js
@@ -3,8 +3,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
-import styled from "styled-components";
-import { Map, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
+import styled, { keyframes } from "styled-components";
+import { Map, TileLayer, GeoJSON, Popup, FeatureGroup } from "react-leaflet";
 import ky from "./data/Kentucky.json";
 import la from "./data/Louisiana.json";
 import sc from "./data/SouthCarolina.json";
@@ -26,44 +26,94 @@ const Styles = styled.div`
   },
 `;
 
-function Edit() {
-  return (
-    <Styles>
-      <Container className="pt-5">
-        <Row>
-          <Col>
-            <Nav variant="pills">
-              <Nav.Item>
-                <Nav.Link eventKey="b1">Create Ghost</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="b2">Connect Precincts</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="b3">Highlight Neighbors</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="b4">Screenshot</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Map center={[34, -85]} zoom={6}>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <GeoJSON data={ky} />
-              <GeoJSON data={la} />
-              <GeoJSON data={sc} />
-            </Map>
-          </Col>
-        </Row>
-      </Container>
-    </Styles>
-  );
+class Edit extends React.Component {
+  state = {
+    lat: 34,
+    lng: -85,
+    zoom: 6
+  };
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { isClicked: false };
+
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleClick() {
+  //   this.setState(state => ({
+  //     isClicked: !state.isClicked
+  //   }));
+  // }
+
+  handleMouseOver(e) {
+    e.target.openPopup();
+  }
+
+  handleMouseOut(e) {
+    e.target.closePopup();
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Styles>
+        <Container className="pt-5">
+          <Row>
+            <Col>
+              <Nav variant="pills">
+                <Nav.Item>
+                  <Nav.Link eventKey="b1">Create Ghost</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="b2">Connect Precincts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="b3">Highlight Neighbors</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="b4">Screenshot</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Map center={position} zoom={this.state.zoom}>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <FeatureGroup>
+                  {ky.features.map(f => {
+                    return (
+                      <GeoJSON
+                        data={f}
+                        // onMouseOver={e => {
+                        //   e.target.openPopup();
+                        // }}
+                        // onMouseOut={e => {
+                        //   e.target.closePopup();
+                        // }}
+                        // onClick={e => {
+                        //   e.target.openPopup();
+                        // }}
+                        onMouseOver={this.handleMouseOver}
+                        onMouseOut={this.handleMouseOut}
+                        // onClick={this.handleClick}
+                      >
+                        <Popup>{f.properties.name}</Popup>
+                      </GeoJSON>
+                    );
+                  })}
+                </FeatureGroup>
+              </Map>
+            </Col>
+          </Row>
+        </Container>
+      </Styles>
+    );
+  }
 }
 
 export default Edit;
--- gui-prototype/src/components/Layout.js
+++ gui-prototype/src/components/Layout.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 
 function Layout(props) {
   return (
-    <Container fluid className="p-5">
+    <Container fluid className="py-5">
       {props.children}
     </Container>
   );
